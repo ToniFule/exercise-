68a8c6ca2d5ca420b86df7e8e247b536c66cec82
ToniFule <toni.fule@gmail.com>
2024-02-18 19:06:41 +0100

    third commit

--- script.js
+++ script.js
@@ -1,10 +1,8 @@
-const firstBtn=document.getElementById("firstBtn")
-const secondBtn=document.getElementById("secondBtn")
-const thirhBtn=document.getElementById("thirdBtn")
-const searchBtn=document.getElementById("searchBtn")
+
 const firstInput=document.getElementById("firstInput1")
 let tHead=document.getElementById("thead")
 let table=document.getElementById("tbl");
+
 
 async function myCountries() {
     try{
@@ -27,11 +25,25 @@ async function fetchCountry() {
    data.forEach(character => {
     const tHead = document.createElement('tr');
     tHead.innerHTML += `
-<td>Flag:<img src="${character.flags.png}" alt="Flag"></td>
-<td>Name:${character.name.official}</td>
-<td>Population:${character.population}</td>
-<td>Capital:${character.capital}</td>
-<td>Area:${character.area}</td>
+<tr>
+<td>Flag:</td>
+<td><img src="${character.flags.png}" alt="Flag"></td>
+</tr>
+<tr>
+<td>Name:</td>
+<td>${character.name.official}</td>
+</tr>
+<td>Population:</td>
+<td>${character.population}</td>
+<tr>
+<td>Capital:</td>
+<td>${character.capital}</td>
+</tr>
+<tr>
+<td>Area:</td>
+<td>${character.area}</td>
+<td>  <p>Open on <a href="https://en.wikipedia.org/wiki/${character.name.common}" >Wikipedia</a></p>
+</tr>
 `;
 
 
@@ -42,5 +54,53 @@ table.appendChild(tHead)
 }
 catch{(err)=>console.log(err)}
 }
+async function usingEuro() {
+    try {
+        const res = await fetch(`https://restcountries.com/v3.1/all`);
+        const data = await res.json();
+        
+        const euroCountries = data.filter(country => {
+            return country.currencies && country.currencies.hasOwnProperty('EUR');
+        });
 
-   
+        const euroCountriesElement = document.getElementById("euroCountries");
+        let euroCountriesHTML = "<ul>";
+        euroCountries.forEach(country => {
+            euroCountriesHTML += `
+            <li><img src="${country.flags.png}" alt="Flag">${country.name.common} is using Euro!! </li>`;
+        });
+        euroCountriesHTML += "</ul>";
+        euroCountriesElement.innerHTML = euroCountriesHTML;
+    } catch (err) {
+        console.error("Error fetching data: ", err);
+    }
+}
+
+async function talkingEnglish() {
+    try {
+        const res = await fetch(`https://restcountries.com/v3.1/all`);
+        const data = await res.json();
+        
+        const englishLanguage = data.filter(country => {
+            return country.languages && country.languages.hasOwnProperty('eng');
+        });
+
+        const englishLanguageElement = document.getElementById("talkingEnglish");
+        let englishLanguageHTML = "<ul>";
+        englishLanguage.forEach(country => {
+            englishLanguageHTML += `
+            <li><img src="${country.flags.png}" alt="Flag">${country.name.common} is talking English!! </li>`;
+        });
+        englishLanguageHTML += "</ul>";
+        englishLanguageElement.innerHTML = englishLanguageHTML;
+    } catch (err) {
+        console.error("Error fetching data: ", err);
+    }
+}
+
+
+function resetBtn() {
+    euroCountries.innerHTML = ""; 
+    englishLanguage.innerHTML="";
+    tHead.innerHTML="";
+}
